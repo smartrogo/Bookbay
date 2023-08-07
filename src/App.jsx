85d585b8420f373d6ab0ace84c5cbb291cf36d21
alajido <96017433+Alajido@users.noreply.github.com>
2023-08-07 21:58:59 +0100

    setting project environment

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
       <h1 className="text-center text-green-600 underline text-2xl">
         HELLO FROM BOOKBAY APP!
       </h1>
-      <h1>buy and sell books online</h1>
+      <h1 className="text-center">buy and sell books online</h1>
     </div>
   );
 }
