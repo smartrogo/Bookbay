93710f7f72fc286b45a03433f6420161977b6049
alajido <96017433+Alajido@users.noreply.github.com>
2023-08-07 02:51:28 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <h1 className=' text-blue-500 text-2xl text-center'>Welcome to Bookbay App.</h1>
+    </>
+  )
+}
+
+export default App
