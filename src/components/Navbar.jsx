37ca4e416ef0ba85a6674a3467f8bb24bd02c3aa
alajido <96017433+Alajido@users.noreply.github.com>
2023-08-09 09:25:05 +0100

    setting project environment

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
     <div className="">
       <nav className="bg-gray-800 shadow-md">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="flex items-center justify-between h-16">
+          <div className="flex items-center border-2 border-white justify-between h-16">
             <div className="flex items-center">
               <div className="flex-shrink-0 text-white">bookbay</div>
             </div>
