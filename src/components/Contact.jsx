261c6df1dc1c7758a838d6a6156bf200a1718962
alajido <96017433+Alajido@users.noreply.github.com>
2023-08-27 01:13:10 +0100

    landing page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,29 +1,72 @@
-import React from 'react'
+import React from "react";
+import { Button } from "./Button";
 
 export const Contact = () => {
   return (
-    <section className="text-gray-600 body-font relative">
-  <div className="container px-5 py-24 mx-auto flex sm:flex-nowrap flex-wrap">
-    <div className="lg:w-1/2 bg-white flex flex-col w-full md:py-8 mt-8 md:mt-0">
-      <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">Feedback</h2>
-      <p className="leading-relaxed mb-5 text-gray-600">Post-ironic portland shabby chic echo park, banjo fashion axe</p>
-      <div className="relative mb-4">
-        <label htmlFor="name" className="leading-7 text-sm text-gray-600">Name</label>
-        <input type="text" id="name" name="name" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
-      </div>
-      <div className="relative mb-4">
-        <label htmlFor="email" className="leading-7 text-sm text-gray-600">Email</label>
-        <input type="email" id="email" name="email" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
-      </div>
-      <div className="relative mb-4">
-        <label htmlFor="message" className="leading-7 text-sm text-gray-600">Message</label>
-        <textarea id="message" name="message" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
-      </div>
-      <button className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
-      <p className="text-xs text-gray-500 mt-3">Chicharrones blog helvetica normcore iceland tousled brook viral artisan.</p>
-    </div>
-  </div>
-</section>
+    <section className="relative mb-10">
+      <div className=" w-[90%] md:w-[75%] px-5 md:px-12 pb-10 mx-auto flex sm:flex-nowrap flex-wrap">
+        <div className="lg:w-1/2 bg-white flex flex-col w-full md:py-8 mt-8 md:mt-0">
+          <h2 className="text-[#000] poppins text-[1.03794rem] md:text-[1.75rem] text-style mb-1 font-bold leading-normal title-font capitalize">
+            contact us
+          </h2>
 
-  )
-}
+          <div className="relative mb-4">
+            <label
+              htmlFor="email"
+              className="leading-normal poppins capitalize text-[0.66725rem] text-[#000] font-normal"
+            >
+              Email:
+            </label>
+
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className="w-full bg-[#eee] rounded-[0.29656rem] md:rounded-[0.5rem] focus:border-[#0F9D58] focus:ring-1 focus:ring-[#abeacc] text-base outline-none text-[#696969] py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              placeholder="example@gmail.com"
+            />
+          </div>
+
+          <div className="relative mb-4">
+            <label
+              htmlFor="name"
+              className="leading-normal poppins capitalize text-[0.66725rem] text-[#000] font-normal"
+            >
+              Name:
+            </label>
+
+            <input
+              type="text"
+              id="name"
+              name="name"
+              className="w-full bg-[#eee] rounded-[0.29656rem] md:rounded-[0.5rem] focus:border-[#0F9D58] focus:ring-1 focus:ring-[#abeacc] text-base outline-none text-[#696969] py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              placeholder="Muhammad Ni'imatullahi "
+            />
+          </div>
+
+          <div className="relative mb-4">
+            <label
+              htmlFor="message"
+              className="leading-normal poppins capitalize text-[0.66725rem] text-[#000] font-normal"
+            >
+              Message
+            </label>
+
+            <textarea
+              id="message"
+              name="message"
+              className="w-full bg-[#eee] rounded-[0.29656rem] md:rounded-[0.5rem] focus:border-[#0F9D58] focus:ring-2 focus:ring-[#abeacc] h-32 text-base outline-none text-[#696969] py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+              placeholder="Your Message"
+            ></textarea>
+          </div>
+
+          <Button
+            value="send"
+            cls_name="bg-[#0F9D58] mx-auto rounded-[0.29656rem] md:rounded-[0.5rem] py-[0.37069rem] px-[0.92675rem] md:py-[0.625rem] md:px-[1.5625rem] text-center text-[#fff] poppins text-[0.66725rem] md:text-[1.125rem] text-style leading-normal font-normal capitalize"
+          />
+        </div>
+      </div>
+      <hr className=" h-[1px] mt-10 mx-auto w-[90%] border-[#333] " />
+    </section>
+  );
+};
