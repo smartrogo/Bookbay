da87ca95d5473a5409a71ba4b0b176330a4e1234
alajido <96017433+Alajido@users.noreply.github.com>
2023-09-06 15:35:24 +0100

    testimony

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
-import { UserProfile } from '@clerk/clerk-react';
+import { UserProfile, useUser } from '@clerk/clerk-react';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 
 export const Profile = () => {
+    const { user } = useUser();
+    const navigate = useNavigate();
+    useEffect(() => {
+        if (!user) {
+          navigate("/sign-in");
+        }
+      }, [user, navigate]);
   return (
     <div>
         <UserProfile />
