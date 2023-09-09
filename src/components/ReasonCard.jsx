1a757b8dfa5562fba62dfd4805840c539ef83486
alajido <96017433+Alajido@users.noreply.github.com>
2023-09-09 16:46:25 +0100

    home page

--- src/components/ReasonCard.jsx
+++ src/components/ReasonCard.jsx
@@ -10,12 +10,12 @@ export const ReasonCard = ({title, body, icon, cls_name, text_color}) => {
       <div className="">
         
         <div className="">
-          <img src={icon} alt="illustration" className="w-[2.88206rem] h-[2.93644rem] mb-2" />
+          <img src={icon} alt="illustration" className="w-[2.88206rem] h-[2.93644rem] md:w-[3.3125rem] md:h-[3.375rem] mb-2" />
           <span style={{color: `${text_color}`}} className=" my-8 icon outfit leading-[2rem] text-[1.30506rem] text-style font-bold">{title}</span>
         </div>
 
       </div>
-      <p className="mt-2 capitalize poppins text-[0.87006rem] font-style font-normal leading-[1.04406rem]">
+      <p className="mt-2 w-[16.15044rem] h-[4.13275rem] md:h-[4.6875rem] md:w-[18.4375rem] capitalize poppins text-[0.87006rem] text-[#333] md:text-[1rem] font-style font-normal leading-[1.04406rem]">
         {body}
       </p>
     </div>
--- src/components/Reasons.jsx
+++ src/components/Reasons.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Button } from "./Button";
-import box from "../assets/box.svg"
-import buy from "../assets/buy.svg"
+import audio from "../assets/audio.svg"
+import buyCart from "../assets/buyCart.svg"
 import reads from "../assets/reads.svg"
-import sound from "../assets/sound.svg"
+import sell from "../assets/sell.svg"
+import secure from "../assets/secure.svg"
 import borrow from "../assets/borrow.svg"
 import { ReasonCard } from "./ReasonCard";
 
@@ -16,15 +17,24 @@ export const Reasons = () => {
         Why use Bookbay ?
       </h1>
 
-      <div className="flex flex-wrap justify-center gap-6 md:justify-evenly md:w-[90%] mx-auto">
+      <div className="border-2 border-red-500 grid grid-cols-1 sm:grid-cols-2 sm:gap-x-6 sm:w-[90%] md:grid-cols-2 lg:grid-cols-3 gap-6 md:w-[90%] md:gap-x-1 lg:w-[95%] lg:gap-x-10 xl:gap-x-2 xl:w-[90%] mx-auto">
         
-        <ReasonCard text_color="#7E4FFD" cls_name="mr block box-sha rounded-[1rem] w-[308px] h-[208.81px] md:w-[20.25rem] md:h-[17.8125rem] my- why mx-auto bg-[#F5F3FF]" icon={reads} title="Read to Earn" body="Is a compelling concept where you can be rewarded for your reading activities. by offering incentives such as tokens, discounts."/>
+        <ReasonCard text_color="#7E4FFD" cls_name="mr block box-sha rounded-[1rem] w-[308px] h-[208.81px] md:w-[21.5625rem] md:h-[13rem] md:p-[0.5rem] my- why mx-auto bg-[#F5F3FF]" icon={reads} title="Read to Earn" body="Is a compelling concept where you can be rewarded for your reading activities. by offering incentives such as tokens, discounts."/>
 
         <ReasonCard text_color="#5098FD"
-        cls_name="mr block box-sha rounded-[1rem] w-[308px] h-[208.81px] md:w-[20.25rem] md:h-[17.8125rem] why mx-auto bg-[#F6F9FE]" icon={borrow} title="Borrow" body="Access a vast library of books, including rare and hard-to-find titles, through our borrowing service, saving you money and space."/>
+        cls_name="mr block box-sha rounded-[1rem] w-[308px] h-[208.81px] md:w-[21.5625rem] md:h-[13rem] md:p-[0.5rem] why mx-auto bg-[#F6F9FE]" icon={borrow} title="Borrow" body="Access a vast library of books, including rare and hard-to-find titles, through our borrowing service, saving you money and space."/>
 
-        <ReasonCard text_color="#5098FD"
-        cls_name="mr block box-sha rounded-[1rem] w-[308px] h-[208.81px] md:w-[20.25rem] md:h-[17.8125rem] why mx-auto bg-[#F6F9FE]" icon={borrow} title="Borrow" body="Access a vast library of books, including rare and hard-to-find titles, through our borrowing service, saving you money and space."/>
+        <ReasonCard text_color="#E7439C"
+        cls_name="mr block box-sha rounded-[1rem] w-[308px] h-[208.81px] md:w-[21.5625rem] md:h-[13rem] md:p-[0.5rem] why mx-auto bg-[#FEF5F8]" icon={buyCart} title="Buy" body="Shop for both new and used books at budget-friendly prices, making it easy to expand your personal collection."/>
+        
+        <ReasonCard text_color="#45D7B5"
+        cls_name="mr block box-sha rounded-[1rem] w-[308px] h-[208.81px] md:w-[21.5625rem] md:h-[13rem] md:p-[0.5rem] why mx-auto bg-[#EEFAF6]" icon={sell} title="Sell" body="Earn money by selling your own books and research papers, turning your bookshelf/research into a potential source of income."/>
+
+        <ReasonCard text_color="#FBAE14"
+        cls_name="mr block box-sha rounded-[1rem] w-[308px] h-[208.81px] md:w-[21.5625rem] md:h-[13rem] md:p-[0.5rem] why mx-auto bg-[#FEFBF2]" icon={audio} title="Audio books" body="Immerse yourself in listening adventures, discover new genres, and enjoy a diverse range of titles, all conveniently accessible."/>
+
+        <ReasonCard text_color="#7E4FFF"
+        cls_name="mr block box-sha rounded-[1rem] w-[308px] h-[208.81px] md:w-[21.5625rem] md:h-[13rem] md:p-[0.5rem] why mx-auto bg-[#EEFAF6]" icon={secure} title="Secure transaction" body="Bookbay prioritizes secure transactions to ensure the safety of your financial information and personal data."/>
 
 
       </div>
