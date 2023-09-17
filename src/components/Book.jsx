8f79b9a56e17518687e65e2b2f531a235e45234c
alajido <96017433+Alajido@users.noreply.github.com>
2023-09-17 23:28:51 +0100

    home page

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -13,14 +13,13 @@ export const Book = ({ bookId, cover, title, year, author, loading }) => {
       <div className="h-[15rem] m-2 bd w-full text-center sm:h-[24rem] lg:h-[20rem] xl:h-[20rem] flex flex-col items-center justify-center ">
 
 
-        <div className="book flex items-center justify-center w-[65%] lg:w-[55%] lg:h-[60%] h-[60%] sm:h-[70%]">
+        <div className="book flex items-center mb-4 justify-center w-[65%] lg:w-[55%] lg:h-[60%] h-[60%] sm:h-[70%]">
           {loading ? (
             <div className="w-full fit">
               <Skeleton
-              baseColor="#202020"
+              baseColor="#D9D9D9"
               height="100%"
-              highlightColor="#444"
-              className="w-ful"
+              highlightColor="#fcfcfc"
             />
             </div>
           ) : (
@@ -36,9 +35,9 @@ export const Book = ({ bookId, cover, title, year, author, loading }) => {
 
         
 
-        <span className="color text-xs md:text-sm mx-auto w-full md:w-[40%]">
+        <span className="color text-xs md:text-sm mx-auto w-full md:w-[70%]">
           {loading ? (
-          <Skeleton className="my-[4px] border-2 ske border-blue-500" baseColor="#202020" highlightColor="#444" />
+          <Skeleton className="mb-[4px] mt-[8px] h-[2rem] ske" baseColor="#D9D9D9" highlightColor="#fcfcfc" />
           ) : (
             <span className="color text-xs md:text-sm text-center mt-4 w-[90%]">
             {title}
@@ -47,7 +46,7 @@ export const Book = ({ bookId, cover, title, year, author, loading }) => {
         </span>
         <span className="color text-xs md:text-sm mx-auto w-full md:w-[80%]">
           {loading ? (
-          <Skeleton className="my-[4px] border-blue-50 ske2" baseColor="#202020" highlightColor="#444" />
+          <Skeleton className="my-[4px] md:my-[2px] h-[2rem] border-blue-50 ske2" baseColor="#D9D9D9" highlightColor="#fcfcfc" />
           ) : (
             <span className="color text-xs md:text-sm text-center mt-4 w-[90%]">
             {`year: ${year}`}
@@ -56,7 +55,7 @@ export const Book = ({ bookId, cover, title, year, author, loading }) => {
         </span>
         <span className="color text-xs md:text-sm text-center w-full md:w-[80%]">
           {loading ? (
-            <Skeleton baseColor="#202020" className="ske3 my-[4px] border-2 border-green-500" highlightColor="#eee" />
+            <Skeleton baseColor="#D9D9D9" className="ske3 h-[2rem] md:h-[3rem] my-[4px] md:my-[2px]" highlightColor="#fcfcfc" />
           ) : (
             author[0]
           )}
