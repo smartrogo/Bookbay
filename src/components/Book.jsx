b8b60c45e8a1f0a2a5e92c89f643929c386c4117
alajido <96017433+Alajido@users.noreply.github.com>
2023-09-18 00:11:43 +0100

    home page

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -13,7 +13,7 @@ export const Book = ({ bookId, cover, title, year, author, loading }) => {
       <div className="h-[15rem] m-2 bd w-full text-center sm:h-[24rem] lg:h-[20rem] xl:h-[20rem] flex flex-col items-center justify-center ">
 
 
-        <div className="book flex items-center mb-4 justify-center w-[65%] lg:w-[55%] lg:h-[60%] h-[60%] sm:h-[70%]">
+        <div className="book flex items-center mb-3 justify-center w-[65%] lg:w-[55%] lg:h-[60%] h-[60%] sm:h-[70%]">
           {loading ? (
             <div className="w-full fit">
               <Skeleton
@@ -37,7 +37,7 @@ export const Book = ({ bookId, cover, title, year, author, loading }) => {
 
         <span className="color text-xs md:text-sm mx-auto w-full md:w-[70%]">
           {loading ? (
-          <Skeleton className="mb-[4px] mt-[8px] ske" baseColor="#D9D9D9" highlightColor="#fcfcfc" />
+          <Skeleton className="mb-[2px] mt-[8px] ske" baseColor="#D9D9D9" highlightColor="#fcfcfc" />
           ) : (
             <span className="color text-xs md:text-sm text-center mt-4 w-[90%]">
             {title}
