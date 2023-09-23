6eede0c6548ca3171092f2eb6794df0390fd4a46
alajido <96017433+Alajido@users.noreply.github.com>
2023-09-23 14:31:07 +0100

    category page

---
+++ src/pages/Test.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import { Web3Button } from '@web3modal/react'
+export const Test = () => {
+  return (
+    <Web3Button />
+  )
+}
--- src/pages/WalletConnect.jsx
+++ src/pages/WalletConnect.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import React from "react";
+import {
+  EthereumClient,
+  w3mConnectors,
+  w3mProvider,
+} from "@web3modal/ethereum";
+import { Web3Modal } from "@web3modal/react";
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
+import { arbitrum, mainnet, polygon } from "wagmi/chains";
+import { Test } from "./Test";
+
+const chains = [arbitrum, mainnet, polygon];
+const projectId = "5e13fa2664931e9ffbca253148dde5b9";
+
+const { publicClient } = configureChains(chains, [w3mProvider({ projectId })]);
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors: w3mConnectors({ projectId, chains }),
+  publicClient,
+});
+const ethereumClient = new EthereumClient(wagmiConfig, chains);
 
 export const WalletConnect = () => {
   return (
-    <div>WalletConnect</div>
-  )
-}
+    <div className="mt-20">
+    <WagmiConfig config={wagmiConfig}>
+        <Test />
+      </WagmiConfig>
+
+      <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
+    </div>
+  );
+};
