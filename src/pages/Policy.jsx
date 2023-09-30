9096907b7e8ec9134e0cc898f454e0b9a06d6cc3
alajido <96017433+Alajido@users.noreply.github.com>
2023-09-30 23:21:35 +0100

    materials page

--- src/pages/Policy.jsx
+++ src/pages/Policy.jsx
@@ -1,25 +1,84 @@
 import React from "react";
+import { Footer } from "../components/Footer";
 
 export const Policy = () => {
   return (
-    <div className="mt-24 h-screen p-4 text-start">
-      <h1 className="text-[#000] outfit text-[1.5rem] text-style font-bold leading-normal">
+    <div className="mt-24 h-screen text-start">
+     
+
+      <div className="mt-4 p-4 w-full mx-auto md:w-[90%]">
+
+      <h1 className="text-[#000] mb-2 outfit text-[1.5rem] text-style font-bold leading-normal">
         Bookbay’s privacy policy{" "}
       </h1>
 
-      <div className="mt-6 w-full md:w-[90%]">
         <h2 className="outfit text-[#00f] text-[1.125rem] font-bold text-style">
           Privacy Policy
         </h2>
 
         <p>
-          Last Updated: 17/09/2023 At Bookbay App we are committed to protecting
+          <span className=" italic">Last Updated: 17/09/2023</span> <br/> At Bookbay App we are committed to protecting
           your privacy and ensuring the security of your personal information.
           This Privacy Policy explains how we collect, use, disclose, and
           safeguard your information when you use our website, services, or
           interact with us.
         </p>
+
+        <div>
+          <p>Information We Collect</p>
+          <ul className=" list-decimal list-inside">
+            <li>
+              Personal Information: We may collect personal information such as
+              your name, email address, shipping address, and payment details
+              when you make a purchase.
+            </li>
+            <li>
+              purchase. Browsing Information: We collect non-personal
+              information, including your IP address, browser type, and browsing
+              behavior on our website.
+            </li>
+            <li>Communication: We may record and store communication, including emails and customer support inquiries.</li>
+          </ul>
+        </div>
+
+        <div>
+          <p>How We Use Your Information:</p>
+          <ul className="list-decimal list-inside">
+            <li>Order Processing: We use your personal information to process orders, provide customer support, and deliver products to you.</li>
+            <li>Personalization: We use browsing information to personalize your experience, show relevant products, and improve our website.</li>
+            <li>Communication: We may use your contact information to send order updates, promotional materials, and newsletters. You can opt out of marketing communications at any time.</li>
+          </ul>
+        </div>
+
+        <div>
+          <p>
+          haring Your Information:
+We do not sell or rent your personal information to third parties. However, we may share your information with:
+</p>
+<ul className=" list-disc list-inside">
+  <li>Shipping and delivery partners.</li>
+  <li> Legal requirements, such as law enforcement agencies.</li>
+  <li>· Payment processors for transaction processing.</li>
+</ul>
+<p>
+Data Security:
+We employ industry-standard security measures to protect your data. However, no online transmission is entirely secure, and we cannot guarantee the security of your information.
+Cookies:
+We use cookies and similar technologies to enhance your browsing experience and collect non-personal information. You can control cookies through your browser settings.
+Your Rights:
+You have the right to access, update, and correct your personal information. You can also request the deletion of your data, subject to legal limitations.
+Children's Privacy:
+Our services are not intended for individuals under 13 years old. We do not knowingly collect or store information from children.
+Changes to This Policy:
+We may update this Privacy Policy periodically. Please review the policy regularly for any changes.
+Contact Us:
+If you have questions or concerns about your privacy or this policy, please contact us at +2348120304001 or <span className="underline">info@bookbay.com.ng  </span>
+ By using our website and services, you consent to the practices described in this Privacy Policy.
+          </p>
+        </div>
       </div>
+
+<Footer />
     </div>
   );
 };
--- src/pages/ServiceTerms.jsx
+++ src/pages/ServiceTerms.jsx
@@ -1,7 +1,198 @@
 import React from 'react'
-
+import { Footer } from '../components/Footer';
 export const ServiceTerms = () => {
   return (
-    <div>ServiceTerms</div>
+    <div className='mt-24 h-screen text-start'>
+      <div className="mt-4 p-4 w-full mx-auto md:w-[90%]">
+        <h1 className="text-[#000] mb-2 outfit text-[1.5rem] text-style font-bold leading-normal">Bookbay’s terms of services </h1>
+
+        <h2 className="outfit text-[#00f] text-[1.125rem] font-bold text-style">Terms and Conditions</h2>
+
+       <div>
+       <p>
+        Please read these terms and conditions carefully before using our book borrowing, buying, and selling services. By accessing or using these services, you agree to comply with and be bound by the following terms and conditions:
+Book Borrowin
+        </p>
+       </div>
+
+       <div>
+        <span className='font-bold text-[1rem]'>
+          a)  Borrower Responsibilities:
+        </span>
+        <ul className='list-inside list-disc'>
+          <li> Borrowers must be registered members.</li>
+          <li> Borrowed books must be returned in the same condition within the specified timeframe.</li>
+          <li> Late returns may result in fines or restrictions on borrowing privileges.</li>
+        </ul>
+       </div>
+       
+       <div>
+        <span className='font-bold text-[1rem]'>
+          b) Library Rules:
+        </span>
+        <ul className='list-inside list-disc'>
+          <li>Borrowers must follow library rules and policies, including handling books with care and maintaining a quiet environment, Book Buying</li>
+        </ul>
+       </div>
+
+       <div>
+        <span className='font-bold text-[1rem]'>
+          c) Ordering and Payment:
+        </span>
+        <ul className='list-inside list-disc'>
+          <li> Orders are subject to availability.</li>
+          <li> Payment must be made in full before the book is shipped.</li>
+        </ul>
+       </div>
+
+       <div>
+        <span className='font-bold text-[1rem]'>
+          d) Returns and Refunds:
+        </span>
+        <ul className='list-inside list-disc'>
+          <li>  Returns are accepted within [3] days of purchase, subject to our return policy.</li>
+          <li> Refunds will be processed according to our refund policy.
+Book Selling</li>
+        </ul>
+       </div>
+
+       <div>
+        <span className='font-bold text-[1rem]'>
+          e)  Listing Books:
+        </span>
+        <ul className='list-inside list-disc'>
+          <li> Sellers must provide accurate book descriptions and conditions.</li>
+          <li>  Prices and shipping details must be clearly stated.</li>
+        </ul>
+       </div>
+
+       <div>
+        <span className='font-bold text-[1rem]'>
+          f)  Transaction Completion:
+        </span>
+        <ul className='list-inside list-disc'>
+          <li> Sellers are responsible for shipping books promptly upon receiving orders.</li>
+          <li>   Payment for sold books is subject to our payment processing procedures.</li>
+        </ul>
+       </div>
+
+       <div>
+        <span className='font-bold text-[1rem]'>
+          g)  Privacy and Data Security:
+        </span>
+        <ul className='list-inside list-disc'>
+          <li>  Users must respect copyright and intellectual property rights when listing, borrowing, or buying books through our platform.</li>
+        </ul>
+       </div>
+
+       <div>
+        <span className='font-bold text-[1rem]'>
+          h)  Termination of Services:
+        </span>
+        <ul className='list-inside list-disc'>
+          <li>We reserve the right to terminate or suspend services and user accounts for violation of these terms and conditions.
+Dispute Resolution:</li>
+          <li>Any disputes will be resolved in accordance with our dispute resolution process.</li>
+        </ul>
+       </div>
+
+       <div>
+
+        <span className='font-bold text-[1rem]'>
+          i)  Modifications:
+        </span>
+        <ul className='list-inside list-disc'>
+          <li>We may update or modify these terms and conditions at any time. Users will be notified of changes.
+By using our book services, you acknowledge that you have read, understood, and agree to abide by these terms and conditions. If you do not agree with any part of these terms, please refrain from using our services.
+</li>
+        </ul>
+       </div>
+
+       <div>
+
+        <span className='font-bold text-[1rem]'>
+          j)  Return Policy
+        </span>
+        <ul className='list-inside list-disc'>
+          <li>Please familiarize yourself with our book buying and borrowing policies, including our return policy, to ensure a smooth and satisfying experience.
+</li>
+        </ul>
+       </div>
+
+       <div className='mt-6'>
+       <h2 className="outfit text-[#00f] text-[1.125rem] font-bold text-style">Book Buying Policy:</h2>
+
+       
+       <div>
+        <span className='font-bold text-[1rem]'>
+        Ordering and Payment:
+        </span>
+        <ul className='list-inside list-decimal'>
+          <li> Orders are subject to availability.</li>
+          <li>  Payment must be made in full before the book is shipped.</li>
+          <li> Prices and shipping details are clearly stated at the time of purchase.</li>
+        </ul>
+       </div>
+
+       <div>
+        <span className='font-bold text-[1rem]'>
+        Returns and Refunds:
+        </span>
+        <ul className='list-inside list-decimal'>
+          <li> Returns are accepted within 3 days of receiving your book.</li>
+          <li> To initiate a return, please contact our customer support team within the specified timeframe.</li>
+          <li> Books must be returned in the same condition as received, with no damage or signs of wear.</li>
+          <li> Shipping costs for returns are the responsibility of the buyer unless the return is due to an error on our part.</li>
+          <li>
+Refunds will be processed promptly upon receiving and inspecting the returned book.</li>
+        </ul>
+       </div>
+       </div>
+
+       <div className='mt-6'>
+       <h2 className="outfit text-[#00f] text-[1.125rem] font-bold text-style">Book Borrowing Policy:</h2>
+
+       
+       <div>
+        <span className='font-bold text-[1rem]'>
+        Borrower Responsibilities:
+        </span>
+        <ul className='list-inside list-decimal'>
+          <li>Borrowers must be registered members of our Platform.</li>
+          <li>  Borrowed books must be returned within the specified borrowing period.</li>
+          <li>Late returns may result in fines or restrictions on borrowing privileges.</li>
+          <li>Borrowers are responsible for the care and safekeeping of borrowed books.</li>
+        </ul>
+       </div>
+
+       <div>
+        <span className='font-bold text-[1rem]'>
+        Return Policy for Borrowed Books:
+        </span>
+        <ul className='list-inside list-decimal'>
+          <li> Borrowed books must be returned in the same condition as when borrowed.</li>
+          <li> Any damage or loss of borrowed books must be reported to the platform promptly.</li>
+          <li> Overdue fines may apply for late returns, as per our Bookbay’s policy.</li>
+        </ul>
+       </div>
+
+       <div>
+        <span className='font-bold text-[1rem]'>
+        General Return Guidelines:
+        </span>
+        <ul className='list-inside list-decimal'>
+          <li> Returns for borrowed books and purchased books are separate processes.</li>
+          <li> Please ensure you review the specific return policy for the type of transaction you are involved in.</li>
+         
+        </ul>
+       </div>
+       <p>We strive to provide excellent service and are committed to resolving any issues promptly and fairly. If you have any questions or concerns about our policies or need assistance with a return, please contact our customer support team for assistance.
+Thank you for choosing our BookbayServices. We look forward to serving you and ensuring your satisfaction with our products and lending library.</p>
+       </div>
+
+      </div>
+
+      <Footer />    
+    </div>
   )
 }
