f59aec3988a991f89c79b2c0632d16b7a6f4abe9
alajido <96017433+Alajido@users.noreply.github.com>
2023-10-09 22:13:35 +0100

    finished delete from cart

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -3,6 +3,7 @@ import { db } from "../firebase";
 import { collection, query, where, getDocs } from "firebase/firestore";
 import { useEffect, useContext, useState } from "react";
 import { AuthContext } from "../AuthContext";
+import { doc, deleteDoc } from "firebase/firestore";
 
 export const Cart = () => {
   const { userData } = useContext(AuthContext);
@@ -17,7 +18,7 @@ export const Cart = () => {
     querySnapshot.forEach((doc) => {
       // doc.data() is never undefined for query doc snapshots
       console.log(doc.id, " => ", doc.data());
-      res = [...res, doc.data()];
+      res = [...res, { ...doc.data(), id: doc.id }];
     });
     setMyCartBooks(res);
   };
@@ -27,6 +28,11 @@ export const Cart = () => {
     console.log("im cart");
   }, []);
 
+  const deleteBook = async (id) => {
+    console.log("id: ", id);
+    await deleteDoc(doc(db, "cart", id));
+    getMyBooks();
+  };
   return (
     <div>
       <br />
@@ -34,14 +40,19 @@ export const Cart = () => {
       <br />
       <br />
       <h1 className="text-2xl text-center">My Cart</h1>
-      <div className="flex justify-between flex-wrap p-6">
+      <div className="flex justify-start flex-wrap p-6 gap-3">
         {myCartBooks.map((item, index) => (
-          <div key={index} className="w-28 border-blue-700 border-2">
-            <h4>{item.title}</h4>
+          <div
+            key={index}
+            className="w-52 border-blue-700 border-2 box-content"
+          >
             <img
               src={`https://covers.openlibrary.org/b/id/${item.cover}-L.jpg`}
               alt={item.title}
             />
+            <h4 className="text">title: {item.title}</h4>
+            <p>author: {item.author}</p>
+            <button onClick={() => deleteBook(item.id)}>delete</button>
           </div>
         ))}
       </div>
