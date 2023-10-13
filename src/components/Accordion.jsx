79507cf642532bb5f176e9ab069479f67b0d7dfb
alajido <96017433+Alajido@users.noreply.github.com>
2023-10-13 18:01:20 +0100

    finished delete from cart

---
+++ src/components/Accordion.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+
+const Accordion = ({ question, answer }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const toggleAccordion = () => {
+    setIsExpanded(!isExpanded);
+  };
+
+  return (
+    <div className=" bg-[#D9D9D9] rounded-[0.5rem] my-4 p-6">
+      <div
+        className="p-4 flex justify-between cursor-pointer"
+        onClick={toggleAccordion}
+      >
+        <div>
+           <p className="text-[#000] outfit text-[1rem] font-normal leading-normal md:text-[1.5rem] capitalize"> {question}</p>
+
+        </div>
+        
+        <div
+          className={`transform transition-transform ${
+            isExpanded ? 'rotate-90' : ''
+          }`}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M19 9l-7 7-7-7"
+            />
+          </svg>
+        </div>
+      </div>
+      <hr className=" h-[1px] mx-auto w-full border-[#333] mt-2 mb-4" />
+      <div
+        className={`overflow-hidden transition-max-h ${
+          isExpanded ? 'max-h-96' : 'max-h-0'
+        }`}
+      >
+        <div className="p-4 mt-4 bg-[#fff] border-l-8 rounded-[0.25rem] border-[#31AF31] mx-auto">
+            <p className='text-[0.875rem] font-normal text-style leading-normal md:text-bold md:text-[1.25rem]'>{answer}</p>
+            </div>
+      </div>
+    </div>
+  );
+};
+
+export default Accordion;
--- src/pages/FAQs.jsx
+++ src/pages/FAQs.jsx
@@ -1,7 +1,40 @@
-import React from 'react'
+import React from 'react';
+import Accordion from '../components/Accordion';
+import { Footer } from '../components/Footer';
 
 export const FAQs = () => {
   return (
-    <div>FAQs</div>
+    <div className='pt-20 h-screen'>
+      <div className='capitalize text-[#000] text-style  leading-normal w-[90%] mx-auto'>
+       <h1 className='text-[1.5rem] md:text-[3rem] font-bold'>Frequently ask questions (FAQS)</h1>
+       <p className='text-[0.875rem] font-normal md:text-[1.25rem]'>may be we can answer you questions here.</p>
+
+
+   <div className='mt-10'>
+   <Accordion
+     question="What is Bookbay?"
+     answer="Answer: Bookbay is a decentralized social networking platform designed to encourage the reading culture in Africa by addressing the challenges associated with textbook borrowing, buying, and selling. It uses a new peer-to-peer protocol to change access to educational resources for students, researchers, and individuals alike."
+    />
+    <Accordion
+     question="How does Bookbay work?"
+     answer="Answer: Bookbay is a peer-to-peer platform, which means that users can directly connect with each other to borrow, buy, and sell textbooks."
+    />
+    <Accordion
+     question="Why use Bookbay ?"
+     answer="Answer:
+     Read to Earn:This is a compelling concept where you can be rewarded for your reading activities. By offering incentives such as tokens, and discounts.
+     Borrow: Access a vast library of books, including rare and hard-to-find titles, through our borrowing service, saving you money and space.
+    ·Buy: Shop for both new and used books at budget-friendly prices, making it easy to expand your personal collection.
+    ·Sell: Earn money by selling your own books and research papers, turning your bookshelf/research into a potential source of income.
+    ·Audiobooks: Immerse yourself in listening adventures, discover new genres, and enjoy a diverse range of titles, all conveniently accessible.
+    ·Secure Transaction:Bookbay prioritizes secure transactions to ensure the safety of your financial information and personal data."
+    />
+   </div>
+
+
+      </div>
+
+<Footer />
+    </div>
   )
 }
