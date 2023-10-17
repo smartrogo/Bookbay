106ed0b2e6771d994c2d9b57292291d56047d0db
alajido <96017433+Alajido@users.noreply.github.com>
2023-10-17 22:07:35 +0100

    finished delete from cart

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -349,8 +349,8 @@ export const Header = () => {
             <div className="border-b-[3px] pb-4 border-[#DBDBDB] flex items-center gap-2 md:gap-4 ">
               <img
                 src={`https://ui-avatars.com/api/?name=${userData?.email
-                  .split("@")[0]
-                  .slice(0, 2)}`}
+                  ?.split("@")[0]
+                  ?.slice(0, 2)}`}
                 alt="profile"
                 className="w-[4rem] h-[4rem] md:w-[5rem] md:h-[5rem] rounded-full"
               />
