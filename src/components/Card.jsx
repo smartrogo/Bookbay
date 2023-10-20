5fe337f1756b1df3dcd070e69fbdf0244fc92e1e
alajido <96017433+Alajido@users.noreply.github.com>
2023-10-20 20:12:15 +0100

    finished delete from cart

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Card = (props) => {
   return (
-    <div className="block mt-10 w-[17.61144rem] h-[17.755rem] md:w-[25.0625rem] md:h-[17.625rem] lg:w-[24rem] lg:h-[18rem] rounded-[1rem] shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:border-success-300 bg-[#eeeeee]">
+    <div className="block mt-10 w-[18.61144rem] h-[19.755rem] md:w-[25.0625rem] md:h-[17.625rem] lg:w-[24rem] lg:h-[18rem] rounded-[1rem] shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:border-success-300 bg-[#eeeeee]">
 
     <div className="px-6 py-3 relative">
       <img src={props.img} alt="testimony img" className='absolute -top-7 left-[6.5rem] md:left-[7rem] lg:left-[9.7rem] w-[5.25rem] h-[5.25rem] tbd rounded-[8.25rem]' />
@@ -10,14 +10,14 @@ export const Card = (props) => {
 
     <div className="p-6 mt-4 m">
 
-      <p className="text-sm poppins w-[16.03069rem] md:w-[17.25rem] lg:w-[17.25rem] relative text-[0.91488rem] md:text-[1rem] italic font-normal leading-[25px] text-center mx-auto text-[#000]">
-          <p className="text-[#666666] font-bold text-[2.19569rem] absolute left-0 -top-1 md:top-1">
+      <p className="text-sm pr-2 md:pr-0 poppins w-[16.03069rem] md:w-[17.25rem] lg:w-[17.25rem] relative text-[0.91488rem] md:text-[1rem] italic font-normal leading-[25px] text-center mx-auto text-[#000]">
+          <span className="text-[#666666] font-bold text-[2.19569rem] absolute left-0 -top-1 md:top-1">
             &#8220;
-           </p>
+           </span>
            <br />
           {props.qoute}
            <br />
-           <span className="absolute -right-2 top-[5rem] md:top-[6rem] text-[#666666] font-bold text-[2.19569rem]">
+           <span className="absolute -right-2 top-[9rem] md:top-[6rem] text-[#666666] font-bold text-[2.19569rem]">
              &#8220;
            </span>
          </p>
