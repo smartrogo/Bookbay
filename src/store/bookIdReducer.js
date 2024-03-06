e68695c68cc33e0b759839703d07526784805f0d
Muhammad Sani Ibrahim <87580367+Saneeitas@users.noreply.github.com>
2024-03-07 00:16:41 +0100

    fix bug

--- src/store/bookIdReducer.js
+++ src/store/bookIdReducer.js
@@ -1,17 +1,21 @@
 // bookIdReducer.js
-const initialState = localStorage.getItem("bookId")
-  ? JSON.parse(localStorage.getItem("bookId"))
-  : [];
+const storedBookIds = localStorage.getItem("bookIds");
+const initialState = storedBookIds ? JSON.parse(storedBookIds) : [];
 
 const bookIdReducer = (state = initialState, action) => {
-  let updatedState;
   switch (action.type) {
     case "SET_BOOK_ID":
-      updatedState = [...state, action.payload];
-      localStorage.setItem("bookId", JSON.stringify(updatedState));
+      // eslint-disable-next-line no-case-declarations
+      const updatedState = [...state, action.payload];
+      localStorage.setItem("bookIds", JSON.stringify(updatedState));
       return updatedState;
+    case "REMOVE_BOOK_ID":
+      // eslint-disable-next-line no-case-declarations
+      const filteredState = state.filter((id) => id !== action.payload);
+      localStorage.setItem("bookIds", JSON.stringify(filteredState));
+      return filteredState;
     case "CLEAR_BOOK_ID":
-      localStorage.removeItem("bookId");
+      localStorage.removeItem("bookIds");
       return [];
     default:
       return state;
